918cf720477a67b990ff024c7861ac6a69120019
alessanderleite <alessander.logistica@gmail.com>
2019-08-03 14:26:07 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/app/SpringBootRestApiOneToManyAssociationApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiOneToManyAssociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiOneToManyAssociationApplication.class, args);
+	}
+
+}
