afd861f6dfac94af4381b33c6adb540ea0df0317
alessanderleite <alessander.logistica@gmail.com>
2019-08-04 12:38:16 -0300

    add the @PutMapping annotation to update changes on database

--- src/main/java/br/com/alessanderleite/app/controller/StudentController.java
+++ src/main/java/br/com/alessanderleite/app/controller/StudentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,7 @@ public class StudentController {
 		return studentRepository.save(student);
 	}
 	
+	@PutMapping("/students/{id}")
 	public Student updateStudent(@PathVariable Long id,
 							@Valid @RequestBody Student studentUpdated) {
 		return studentRepository.findById(id)
